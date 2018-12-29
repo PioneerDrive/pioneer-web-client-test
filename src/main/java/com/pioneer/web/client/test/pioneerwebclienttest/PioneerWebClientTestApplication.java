6401702a759567287536dcb1e1abf3937bf0bc0a
Ben Gonzales <bgonzales20001@gmail.com>
2018-12-29 16:14:31 -0600

    Init repo

---
+++ src/main/java/com/pioneer/web/client/test/pioneerwebclienttest/PioneerWebClientTestApplication.java
@@ -0,0 +1,14 @@
+package com.pioneer.web.client.test.pioneerwebclienttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PioneerWebClientTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PioneerWebClientTestApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/pioneer/web/client/test/pioneerwebclienttest/PioneerWebClientTestApplicationTests.java
@@ -0,0 +1,17 @@
+package com.pioneer.web.client.test.pioneerwebclienttest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PioneerWebClientTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
